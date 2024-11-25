958deba43a7303372d13937f0c73e492a15636c7
Ajyol <ajyold42@gmail.com>
2024-11-25 11:30:01 -0600

    User Service

--- src/main/java/com/theboys/scheduler/service/IUserService.java
+++ src/main/java/com/theboys/scheduler/service/IUserService.java
@@ -1,13 +1,12 @@
 package com.theboys.scheduler.service;
 
 import com.theboys.scheduler.dto.UserDto;
-import com.theboys.scheduler.entity.User;
 
 import java.util.List;
 
 public interface IUserService {
     List<UserDto> findAll();
     UserDto findById(int theId);
-    UserDto save(User theUser);
+    UserDto save(UserDto theUser);
     void deleteById(int theId);
 }
--- src/main/java/com/theboys/scheduler/service/UserService.java
+++ src/main/java/com/theboys/scheduler/service/UserService.java
@@ -1,4 +1,47 @@
 package com.theboys.scheduler.service;
 
-public class UserService {
+import com.theboys.scheduler.dao.UserRepository;
+import com.theboys.scheduler.dto.UserDto;
+import com.theboys.scheduler.entity.User;
+import com.theboys.scheduler.exception.UserNotFoundException;
+import com.theboys.scheduler.mapper.EntityDtoMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService implements IUserService{
+
+    private final EntityDtoMapper entityDtoMapper;
+    private final UserRepository userRepository;
+
+    public UserService(EntityDtoMapper entityDtoMapper, UserRepository userRepository) {
+        this.entityDtoMapper = entityDtoMapper;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserDto> findAll() {
+        List<User> users = userRepository.findAll();
+        return entityDtoMapper.mapUserToDtoList(users);
+    }
+
+    @Override
+    public UserDto findById(int theId) {
+        Optional<User> user = userRepository.findById(theId);
+        return user.map(entityDtoMapper::mapUserToDto)
+                .orElseThrow(() -> new UserNotFoundException("User with Id " + theId + " not found."));
+    }
+
+    @Override
+    public UserDto save(UserDto theUser) {
+        User user = entityDtoMapper.mapUserDtoToEntity(theUser);
+        user = userRepository.save(user);
+        return entityDtoMapper.mapUserToDto(user);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        if (!userRepository.existsById(theId));
+
+    }
 }
